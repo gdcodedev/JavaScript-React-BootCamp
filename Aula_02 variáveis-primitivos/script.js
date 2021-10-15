a5a0dde4ed6befe604ff7d08f8478a7326163988
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-10-15 01:48:51 -0300

    Update
    Update;

---
+++ Aula_02 variáveis-primitivos/script.js
@@ -0,0 +1,14 @@
+// Tipos primitivos
+
+// Boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+// Number
+
+var NumeroQualquer = 1;
+console.log(typeof(NumeroQualquer));
+
+// String 
+var Nome = "Guilherme";
+console.log(typeof(Nome));
