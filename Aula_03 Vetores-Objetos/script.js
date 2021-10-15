649cc9ca04dae6a495b0f29a73f78a09886e6a50
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-10-15 16:32:21 -0300

    Finalizando Arrays
    End Arrays

--- Aula_03 Vetores-Objetos/script.js
+++ Aula_03 Vetores-Objetos/script.js
@@ -8,4 +8,34 @@ let array2 = ["String", 1, true, ["Array2teste"], ["Array2teste2"], ["Array2test
 console.log(array2);
 
 // Manipulando Arrays
-// Ao ser declarado, o Arrat traz consigo uma série de métodos para manipula-lo.
+// Ao ser declarado, o Arrat traz consigo uma série de métodos #1 para manipula-lo.
+
+// forEach() - itera um array;
+array.forEach(function(item, index){console.log(item, index)});
+
+// push() - add item no final do array;
+array.push("novo item");
+console.log(array);
+
+// pop() - remove item no final do array;
+array.pop();
+console.log(array);
+
+// shift() - remove item no início do array;
+array.shift();
+console.log(array);
+
+// unshift() - add item no início do array;
+array.unshift("Novo item no inicio");
+console.log(array);
+
+// indexOf() - retorna o índice de um valor;
+console.log(array.indexOf(true));
+
+// splice() - remove ou substitui unm item pelo índice;
+array.splice(0, 3);
+console.log(array);
+
+// slice() - retorna uma parte de um array existente;
+let novoArray = array.slice(0, 3);
+console.log(novoArray);
