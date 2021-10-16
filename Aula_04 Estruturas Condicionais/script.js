8ee9a8fa14c0938a408bf4c7d26e5fa93476d833
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-10-15 23:43:52 -0300

    if / else if / switch / case
    if / else if / switch / case

---
+++ Aula_04 Estruturas Condicionais/script.js
@@ -0,0 +1,46 @@
+var jogador1 = 0;
+var jogador2 = 0;
+    if (jogador1 > 0){
+      console.log('Jogador 1 Marcou um ponto!');
+    }else if(jogador2 > 0){
+      console.log('O jogador 2 marcou ponto!');
+      } else {
+        console.log('Os jogadores estão empatados!');
+      }
+    
+    // Aninhamento de ifs
+    // Podemos também usar o "if" dentro de um outro "if", chamamos isso de aninhamento de if's ou ninho de if's.
+    //Ex : 
+
+    if(jogador1 = -1) {
+      if(jogador1 > 0) {
+        console.log('Jogador 1 marcou ponto');
+      }else {
+        console.log('Ninguém marcou ponto!');
+      }
+    }
+
+    // IF Ternário 
+    // Podemos também fazer uma verficação em uma única linha usando o 'if' ternário;
+    //Ex :
+
+    var jogador11 = 0;
+    var jogador21 = 0;
+
+    // Usando ternário
+    jogador11 != -1 && jogador21 != -1 ? console.log('Os jogadores são válidos') : 
+    console.log('Jogadores inválidos');
+
+    // Usando if
+    if(jogador11 > 0) {
+      console.log('Jogador 11 marcou ponto!');
+
+      // Usando if else
+    } else if (jogador21 > 0){
+      console.log('Jogador21 marcou ponto!');
+    }else {
+      console.log('Ninguém marcou ponto!');
+    }
+
+    // Usando switch/case 
+    // O switch/case, funciona como uma estrutura condicional também;
