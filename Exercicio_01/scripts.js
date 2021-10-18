f14f9d602d7494416b0716c46e8c2863e8820cdb
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-10-17 23:12:49 -0300

    Exercicio 01
    Aspas duplas, Aspas simples, Template literals

---
+++ Exercicio_01/scripts.js
@@ -0,0 +1,6 @@
+// Texto com Aspas duplas
+console.log("Hello World");
+// Texto com aspas Simples
+console.log('Meu nome é Guilherme');
+// Texto com template literals
+console.log(`Este aqui é um template literals`);
