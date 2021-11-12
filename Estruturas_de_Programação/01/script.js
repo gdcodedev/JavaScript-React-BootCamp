ccb847b5ebfc96225aed9dd4c10ee1f5c7a7c85e
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-11-12 18:58:47 -0300

    Estruturas de programação

---
+++ Estruturas_de_Programação/01/script.js
@@ -0,0 +1,25 @@
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+console.log(`Temos ${laranjas} laranjas na cesta`);
+
+// Um let definindo várias variáveis ao mesmo tempo.
+let quadrado = 4, triangulo = 3, retangulo = 4;
+
+console.log(quadrado * (triangulo + retangulo));
+
+let nome = "Guilherme";
+
+// Utilizando a variável nome
+console.log(nome);
+
+//Interpolando a variável "nome"
+console.log(`Meu nome é ${nome}`);
+
+var nome1 = "Guilherme";
+
+const sobrenome = "Cardoso";
+
+nome = "Pedro";
+
+console.log(`Meu nome é ${nome} ${ sobrenome}`);
